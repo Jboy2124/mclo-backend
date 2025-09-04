2c80495685a7aa7b1d6ab68845af4f04128979fc
Jboy Abecia <jboyabecia@gmail.com>
2025-09-05 00:48:49 +0800

    Additional features

--- modules/services/processing.js
+++ modules/services/processing.js
@@ -7,10 +7,58 @@ module.exports = {
         assigned_to: data.assignedTo,
         recommendations: data.recommendations,
         remarks: data.remarks,
+        date_assigned: new Date(),
         process_status: "Assigned",
       })
       .where("doc_id", data.docId);
 
     return result;
+  },
+  getAssignedProcessedDocuments: async (assigneeId, pageNumber) => {
+    try {
+      const page = pageNumber || 1;
+      const pageSize = 15;
+      const pageOffset = (page - 1) * pageSize;
+
+      const [{ count }] = await knex("tbl_processing_details as proc")
+        .leftJoin("tbl_document_details as doc", "doc.doc_id", "proc.doc_id")
+        .where("proc.process_status", "Assigned")
+        .whereRaw("FIND_IN_SET(?, REPLACE(proc.assigned_to, ' ', ''))", [
+          assigneeId,
+        ])
+        .count({ count: "*" });
+
+      let result = await knex("tbl_processing_details as proc")
+        .select({
+          processId: "proc.process_id",
+          docId: "proc.doc_id",
+          codeId: "docs.code_id",
+          docType: "docs.doc_type",
+          description: "docs.document_description",
+          path: "docs.document_path",
+          additionalPath: "docs.additional_document_path",
+          date_assigned: "proc.date_assigned",
+          recommendations: "proc.recommendations",
+          remarks: "proc.remarks",
+        })
+        .leftJoin("tbl_document_details as docs", "proc.doc_id", "docs.doc_id")
+        .where("proc.process_status", "Assigned")
+        .whereRaw("FIND_IN_SET(?, REPLACE(proc.assigned_to, ' ', ''))", [
+          assigneeId,
+        ])
+        .orderBy("proc.process_id", "desc")
+        .limit(pageSize)
+        .offset(pageOffset);
+
+      result = result.map((row) => ({ ...row, assigneeId }));
+      return {
+        status: "SUCCESS",
+        result,
+        message: "",
+        totalRecords: count,
+      };
+    } catch (error) {
+      return { status: "ERROR", result: [], message: error.message };
+    }
   },
 };
